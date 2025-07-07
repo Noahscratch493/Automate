4687e628a5b5f2063e126831cc5a8cc913ee46bb
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2025-07-07 07:45:35 +0100

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -2383,7 +2383,7 @@ function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) r
 
 
 
-const PACKAGER_URL = 'https://cattycode.github.io/packager/';
+const PACKAGER_URL = 'https://cattymod.github.io/packager/';
 const PACKAGER_ORIGIN = "https://cattymod.github.io/";
 const readBlobAsArrayBuffer = blob => new Promise((resolve, reject) => {
   const fr = new FileReader();
