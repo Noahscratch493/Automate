c17d01615b9c331cac2a17c9e8a47b7167eaca15
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2025-07-07 18:18:53 +0100

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -3116,9 +3116,9 @@ const Footer = () => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.c
   defaultMessage: "Feedback & Bugs",
   id: "tw.feedback"
 })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("a", {
-  href: "CattyCode"
+  href: "CattyMod Jr"
 }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(react_intl__WEBPACK_IMPORTED_MODULE_5__["FormattedMessage"], {
-  defaultMessage: "https://cattycode.github.io",
+  defaultMessage: "https://cattymod.github.io/jr",
   id: "tw.code"
 })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("a", {
   href: "privacy.html"
